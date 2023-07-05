911350dc7b2931bfeae0ac845bb090b3f9782a26
HubertTW <boney.chang@gmail.com>
2023-07-05 18:34:24 +0000

    decryptCount

---
+++ testing/counterTest.cpp
@@ -0,0 +1,60 @@
+#include "binfhecontext.h"
+
+using namespace lbcrypto;
+
+int main() {
+    // Sample Program: Step 1: Set CryptoContext
+
+    auto cc = BinFHEContext();
+
+    cc.GenerateBinFHEContext(TOY);
+
+    // Generate the secret key
+    auto sk = cc.KeyGen();
+
+    std::cout << "Generating the bootstrapping keys..." << std::endl;
+
+    // Generate the bootstrapping keys (refresh and switching keys)
+    cc.BTKeyGen(sk);
+
+    std::cout << "Completed the key generation." << std::endl;
+
+    lbcrypto::LWECiphertext count[8]; /* cmpResult */
+
+    int data[8] = {1,0,0,0,1,1,1,1};
+    lbcrypto::LWECiphertext cmpRes[8];
+
+    for (int i = 0; i < 8; i++ ){
+        cmpRes[i] = cc.Encrypt(sk, data[i]);
+        
+        printf("count %d", i);
+        auto carry  = cmpRes[i] ;
+        for (int lsb = 0; lsb < 8; lsb++) {
+            auto res = cc.EvalBinGate(XOR, count[lsb], carry); /* 0:true,1:false */
+            carry    = cc.EvalBinGate(AND, count[lsb], carry); /* once the temp = false, it's always false */
+            count[lsb]  = res;
+        }
+
+    }
+
+        puts("eval completed");
+
+    
+
+        /* client  only */
+
+        LWEPlaintext result[8];
+        int sum   = 0;
+        int power = 1; /* 2^0 */
+        for (int lsb = 0; lsb < 8; lsb++) {
+            cc.Decrypt(sk, count[lsb], &result[lsb]);
+            sum += (power * result[lsb]);
+
+            power = power * 2;
+        }
+        std::cout << "the sum is : " << sum << std::endl;
+
+        return 0;
+    }
+
+
